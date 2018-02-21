1b2b13aa6321c0e893c84d359cfe5755bf42dca0
zhvkgj <zhvkgj@yandex.ru>
2018-02-21 23:07:23 +0300

    task4

---
+++ Course2/Sem4/HW1/task4/task4/Program.fs
@@ -0,0 +1,12 @@
+﻿let rec pow2 n = 
+    if n = 1 then 2
+    else 2 * pow2 (n-1)
+
+let listBuilder n m =
+    let value = pow2 n
+    let rec listTemp temp m =
+        match m with
+        |_ when m < 0 -> []
+        |_ when m >= 0 -> temp :: listTemp (temp * 2) (m - 1)
+    listTemp value m
+    
