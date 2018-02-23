b384ccb9fca2730a5a043f3417693e419e978036
zhvkgj <zhvkgj@yandex.ru>
2018-02-23 21:31:56 +0300

    add task2

---
+++ course2/sem4/hw1/task2/task2/Program.fs
@@ -0,0 +1,9 @@
+﻿let fibonacciNum N =
+    let rec fibonacci N i prevFirstValue prevSecondValue =
+        if i = N then 
+            prevSecondValue
+        else 
+            fibonacci N (i + 1) (prevFirstValue + prevSecondValue) prevFirstValue
+    fibonacci N 1 1 0
+    
+printfn "%d" <| fibonacciNum 5
