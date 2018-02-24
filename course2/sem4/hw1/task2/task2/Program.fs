6e3462acdacd0ba27f0046318a6d61512ff62971
zhvkgj <zhvkgj@yandex.ru>
2018-02-24 15:31:25 +0300

    fixed task2

--- course2/sem4/hw1/task2/task2/Program.fs
+++ course2/sem4/hw1/task2/task2/Program.fs
@@ -1,9 +1,12 @@
-﻿let fibonacciNum N =
-    let rec fibonacci N i prevFirstValue prevSecondValue =
-        if i = N then 
-            prevSecondValue
-        else 
-            fibonacci N (i + 1) (prevFirstValue + prevSecondValue) prevFirstValue
-    fibonacci N 1 1 0
+﻿let fibonacciNum n =
+    let rec fibonacci n i prevFirstValue prevSecondValue =
+        match n with
+        | _ when n > 0 ->
+            if i = n then 
+                prevSecondValue
+            else 
+                fibonacci n (i + 1) (prevFirstValue + prevSecondValue) prevFirstValue
+        | _ -> -1
+    fibonacci n 1 1 0
     
 printfn "%d" <| fibonacciNum 5
