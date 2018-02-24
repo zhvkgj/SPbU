3f199a135648caa09e8e9ee0dc0580abcb5a2276
zhvkgj <zhvkgj@yandex.ru>
2018-02-24 15:19:21 +0300

    fixed task1

--- course2/sem4/hw1/task1/Task1/Program.fs
+++ course2/sem4/hw1/task1/Task1/Program.fs
@@ -1,4 +1,7 @@
 ﻿let rec factorial n = 
     match n with
-    | 0 -> 1
-    | _ -> n * factorial (n - 1)
+    | _ when n < 0 -> -1
+    | _ when n > 0 -> n * factorial (n - 1)
+    | _  -> 1
+
+printfn "%d" (factorial 3)
