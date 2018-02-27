446e054f436da438cc39fa71ce2d86dfb4df3fe7
zhvkgj <zhvkgj@yandex.ru>
2018-02-27 19:37:56 +0300

    add task1 hw3

---
+++ course2/sem4/hw3/hw3/Program.fs
@@ -0,0 +1,15 @@
+﻿//3.1
+ let firstEvenSearch myList =
+   myList
+   |> List.map (fun x -> (x + 1) % 2)
+   |> List.sum 
+
+let secondEvenSearch myList =
+    myList
+    |> List.filter (fun x -> x % 2 = 0)
+    |> List.length
+
+let thirdEvenSearch myList =
+    List.fold (fun acc x -> if (x % 2 = 0) then (acc + 1) else acc) 0 myList
+
+//3.2
