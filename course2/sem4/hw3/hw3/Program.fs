4e97bd15de1e46af7c1dca661a5e54a5d42575d5
zhvkgj <zhvkgj@yandex.ru>
2018-02-28 14:06:38 +0300

    add task2

--- course2/sem4/hw3/hw3/Program.fs
+++ course2/sem4/hw3/hw3/Program.fs
@@ -14,17 +14,25 @@ let thirdEvenSearch myList =
 
 //3.2
 type Tree<'a> =
-    | Tree of 'a * Tree<'a> * Tree<'a>
+    | Node of 'a * Tree<'a> * Tree<'a>
     | Tip of 'a
 
+let rec treeFuncTR tree cont=
+        match tree with 
+        | Node(x , left, right) ->  cont x
+                                    treeFuncTR left cont
+                                    treeFuncTR right cont
+        | Tip x -> cont x
+
+treeFuncTR (Node(0, Node(1, Tip(2), Tip(3)), Tip(4))) (fun x -> printfn "%d" x)
 
 //3.3
 type ArithmeticExpression =
     | Number of int
-    | Addition of  ArithmeticExpression * ArithmeticExpression      //+
-    | Subtraction of ArithmeticExpression * ArithmeticExpression    //-
-    | Multiplication of ArithmeticExpression * ArithmeticExpression //*
-    | Division of ArithmeticExpression * ArithmeticExpression       // /
+    | Addition of  ArithmeticExpression * ArithmeticExpression     
+    | Subtraction of ArithmeticExpression * ArithmeticExpression    
+    | Multiplication of ArithmeticExpression * ArithmeticExpression 
+    | Division of ArithmeticExpression * ArithmeticExpression       
 
 let rec eval (e: ArithmeticExpression) =
     match e with
@@ -32,9 +40,11 @@ let rec eval (e: ArithmeticExpression) =
     | Addition(e1, e2) -> eval e1 + eval e2
     | Subtraction(e1, e2) -> eval e1 - eval e2
     | Multiplication(e1, e2) -> eval e1 * eval e2
-    | Division(e1, e2) -> eval e1 / eval e2
+    | Division(e1, e2) -> match (eval e2) with 
+                          | _ when eval e2 <> 0 -> eval e1 / eval e2
+                          | _ -> failwith "Divisor cannot be zero!"
 
-printfn "%d" <| eval (Addition(Number(1), Number(2)))
+printfn "%d" <| eval (Division(Number(6), Number(3)))
 //3.4
 let checkIsPrime num =
     let maxNum = sqrt (float num)
