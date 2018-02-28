8b289b158d2b864365ff211d6f19b667837e3ec5
zhvkgj <zhvkgj@yandex.ru>
2018-02-28 20:24:38 +0300

    add unit tests

---
+++ course2/sem4/hw3/tasks/Library.fs
@@ -0,0 +1,62 @@
+namespace hw3
+
+//3.1
+module Task1 =
+    let firstEvenSearch myList =
+        myList
+        |> List.map (fun x -> if (x % 2 = 0) then 1 else 0)
+        |> List.sum 
+
+    let secondEvenSearch myList =
+        myList
+        |> List.filter (fun x -> x % 2 = 0)
+        |> List.length
+
+    let thirdEvenSearch myList =
+        List.fold (fun acc x -> if (x % 2 = 0) then (acc + 1) else acc) 0 myList
+
+//3.2
+module Task2 =
+    type Tree<'a> =
+    | Node of 'a * Tree<'a> * Tree<'a>
+    | Tip of 'a 
+
+    let rec treeFuncTR tree cont=
+        match tree with 
+        | Node(x , left, right) ->  Node (cont x, treeFuncTR left cont, treeFuncTR right cont)                           
+        | Tip x -> Tip(cont x)
+
+//3.3
+module Task3 =
+    type ArithmeticExpression =
+        | Number of int
+        | Addition of  ArithmeticExpression * ArithmeticExpression     
+        | Subtraction of ArithmeticExpression * ArithmeticExpression    
+        | Multiplication of ArithmeticExpression * ArithmeticExpression 
+        | Division of ArithmeticExpression * ArithmeticExpression       
+
+    let rec eval (e: ArithmeticExpression) =
+        match e with
+        | Number(e1) -> e1
+        | Addition(e1, e2) -> eval e1 + eval e2
+        | Subtraction(e1, e2) -> eval e1 - eval e2
+        | Multiplication(e1, e2) -> eval e1 * eval e2
+        | Division(e1, e2) -> match (eval e2) with 
+                              | _ when eval e2 <> 0 -> eval e1 / eval e2
+                              | _ -> failwith "Divisor cannot be zero!"
+
+//3.4
+module Task4 =
+    let checkIsPrime num =
+        let maxNum = sqrt (float num)
+        let rec checkTR (curNum : int) = 
+            match curNum with
+            | _ when curNum > (int maxNum) && maxNum <> 1.0 -> true
+            | _ when (num % curNum <> 0) -> checkTR (curNum + 1)
+            | _ -> false
+       
+        checkTR 2
+
+    let buildInfPrimeSeq = 
+        Seq.initInfinite (fun index -> index + 1)
+        |> Seq.filter (fun x -> checkIsPrime x)
