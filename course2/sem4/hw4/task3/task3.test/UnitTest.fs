89804301062e43a116f6cc47985e550f96827698
zhvkgj <zhvkgj@yandex.ru>
2018-03-19 19:46:03 +0300

    add task3 hw4

---
+++ course2/sem4/hw4/task3/task3.test/UnitTest.fs
@@ -0,0 +1,62 @@
+﻿module Tests
+
+open NUnit.Framework
+open hw4
+open Task3
+
+//4.3 testing
+[<Test>]
+let ``First simple search for free variable`` () =
+  let expected = ['x']
+  let actual = searchFV(Var('x'))
+  Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``Second simple search for free variable`` () =
+  let expected = ['y']
+  let actual = searchFV(Var('x'))
+  Assert.AreNotEqual(expected, actual)
+
+[<Test>]
+let ``Third simple search for free variable`` () = 
+    let expected = []
+    let actual = searchFV (LambdaAbstr ('x', Var ('x')))
+    Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``Advanced search test`` () = 
+    let expected = ['z']
+    let actual = searchFV (App (LambdaAbstr ('x', LambdaAbstr ('y', Var ('x'))), LambdaAbstr ('x', App (Var ('z'), Var ('x')))))
+    Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``First test for substitution`` () = 
+    let expected = (Var ('y'))
+    let actual = subst (Var ('y')) ('x') (Var ('z'))
+    Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``Second test for substitution `` () = 
+    let S = LambdaAbstr ('y', Var ('x'))
+    let T = LambdaAbstr ('s', Var ('y'))
+    let actual = subst S 'x' T
+    let expected = LambdaAbstr ('a', LambdaAbstr ('s', Var ('y')))
+    Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``Test for term "(λx y.x) y u" which should return 'y'`` () =
+  let expected = Var('y')
+  let actual = betaRdct(App(LambdaAbstr('x', App(LambdaAbstr('y', Var('x')), Var('u'))), Var('y')))
+  Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``(Id)(Id) should return Id`` () =
+  let expected = LambdaAbstr('x', Var('x'))
+  let actual = betaRdct(App(LambdaAbstr('x', Var('x')), LambdaAbstr('x', Var('x'))))
+  Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``Beta-reduction with SKK should return I`` () =
+  let expected = LambdaAbstr('b', Var('b'))
+  let actual = betaRdct(betaRdct(App(LambdaAbstr('x', App(LambdaAbstr('y', LambdaAbstr('z', App(App(Var('x'), Var('z')), App(Var('y'), Var('z'))))), LambdaAbstr('x', LambdaAbstr('y', Var('x'))))), LambdaAbstr('x', LambdaAbstr('y', Var('x'))))))
+  Assert.AreEqual(expected, actual)
---
+++ course2/sem4/hw4/task3/task3/Program.fs
@@ -0,0 +1,57 @@
+﻿namespace hw4
+
+type lambdaExp =
+  | Var of char
+  | LambdaAbstr of char * lambdaExp
+  | App of lambdaExp * lambdaExp
+
+module Task3 =
+ (* Declaring a function which extracts the list 
+    of free variables from a lambda term *)
+  let searchFV term =
+    let rec searchingFV term acc =
+      match term with
+      | Var(var) -> var::acc
+      | LambdaAbstr(var, body) -> List.filter 
+                                   (fun cur -> cur <> var) 
+                                    (searchingFV body acc)
+      | App(L, R) -> let leftPart = searchingFV L acc
+                     let rightPart = searchingFV R acc
+                     List.append leftPart rightPart
+    searchingFV term []
+
+  (* Declaring a function for substituting 
+     all free occurrences of a variable 
+     x in a term t with the term t *)
+  let rec subst S x T =
+    match S with
+    | Var(var) -> match var with
+                  | _ when var = x -> T
+                  | _ -> S
+    | LambdaAbstr(var, body) -> let FreeVar = List.append 
+                                               (searchFV body) 
+                                                (searchFV T)
+                                match var with
+                                | _ when var = x -> S
+                                | _ -> let newVar = ['a'..'z']
+                                                    |> List.filter (fun curChar -> not (List.contains curChar FreeVar))
+                                                    |> List.head 
+                                       LambdaAbstr(newVar, subst (subst body var (Var(newVar))) x T)
+    | App(L, R) -> App(subst L x T, subst R x T)
+
+  // Declaring a function for beta-reduction
+  let rec betaRdct t =
+    match t with
+    | Var(x) -> Var(x)
+    | LambdaAbstr(var, body) -> LambdaAbstr(var, betaRdct body)
+    | App(LambdaAbstr(var, body), T) -> betaRdct(subst body var T)
+    | App(L, R) -> App(betaRdct L, betaRdct R)
+    
+    
+
+
+
+    
+
+
+
