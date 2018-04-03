0476bb5901b2416d6cad2ddc023d8edb136becc3
zhvkgj <zhvkgj@yandex.ru>
2018-04-04 01:54:34 +0300

    add task3

--- course2/sem4/hw4/task3/task3.test/UnitTest.fs
+++ course2/sem4/hw4/task3/task3.test/UnitTest.fs
@@ -8,55 +8,55 @@ open Task3
 [<Test>]
 let ``First simple search for free variable`` () =
   let expected = ['x']
-  let actual = searchFV(Var('x'))
+  let actual = getFV(Var('x'))
   Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Second simple search for free variable`` () =
   let expected = ['y']
-  let actual = searchFV(Var('x'))
+  let actual = getFV(Var('x'))
   Assert.AreNotEqual(expected, actual)
 
 [<Test>]
 let ``Third simple search for free variable`` () = 
-    let expected = []
-    let actual = searchFV (LambdaAbstr ('x', Var ('x')))
+    let (expected: char list) = []
+    let actual = getFV (LambdaAbstr (Var ('x'), 'x'))
     Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Advanced search test`` () = 
     let expected = ['z']
-    let actual = searchFV (App (LambdaAbstr ('x', LambdaAbstr ('y', Var ('x'))), LambdaAbstr ('x', App (Var ('z'), Var ('x')))))
+    let actual = getFV (App (LambdaAbstr (LambdaAbstr (Var ('x'), 'y'), 'x'), LambdaAbstr (App (Var ('z'), Var ('x')), 'x')))
     Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``First test for substitution`` () = 
     let expected = (Var ('y'))
-    let actual = subst (Var ('y')) ('x') (Var ('z'))
+    let actual = subst (Var ('y')) (Var ('z')) ('x')
     Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Second test for substitution `` () = 
-    let S = LambdaAbstr ('y', Var ('x'))
-    let T = LambdaAbstr ('s', Var ('y'))
-    let actual = subst S 'x' T
-    let expected = LambdaAbstr ('a', LambdaAbstr ('s', Var ('y')))
+    let S = LambdaAbstr (Var ('x'), 'y')
+    let T = LambdaAbstr (Var ('y'), 's')
+    let actual = subst S T 'x'
+    let expected = LambdaAbstr (LambdaAbstr (Var ('y'), 's'), 'a')
     Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Test for term "(λx y.x) y u" which should return 'y'`` () =
   let expected = Var('y')
-  let actual = betaRdct(App(LambdaAbstr('x', App(LambdaAbstr('y', Var('x')), Var('u'))), Var('y')))
+  let actual = eval(App(LambdaAbstr(App(LambdaAbstr(Var('x'), 'y'), Var('u')), 'x'), Var('y')))
   Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``(Id)(Id) should return Id`` () =
-  let expected = LambdaAbstr('x', Var('x'))
-  let actual = betaRdct(App(LambdaAbstr('x', Var('x')), LambdaAbstr('x', Var('x'))))
+  let expected = LambdaAbstr(Var('x'), 'x')
+  let actual = eval(App(LambdaAbstr(Var('x'), 'x'), LambdaAbstr(Var('x'), 'x')))
   Assert.AreEqual(expected, actual)
 
 [<Test>]
 let ``Beta-reduction with SKK should return I`` () =
-  let expected = LambdaAbstr('b', Var('b'))
-  let actual = betaRdct(betaRdct(App(LambdaAbstr('x', App(LambdaAbstr('y', LambdaAbstr('z', App(App(Var('x'), Var('z')), App(Var('y'), Var('z'))))), LambdaAbstr('x', LambdaAbstr('y', Var('x'))))), LambdaAbstr('x', LambdaAbstr('y', Var('x'))))))
+  let expected = LambdaAbstr(Var('z'), 'z')
+  let actual = eval(App(LambdaAbstr(App(LambdaAbstr(LambdaAbstr(App(App(Var('x'), Var('z')), App(Var('y'), Var('z'))), 'z'), 'y'), LambdaAbstr(LambdaAbstr(Var('x'), 'y'), 'x')), 'x'), LambdaAbstr(LambdaAbstr(Var('x'), 'y'), 'x')))
   Assert.AreEqual(expected, actual)
--- course2/sem4/hw4/task3/task3/Program.fs
+++ course2/sem4/hw4/task3/task3/Program.fs
@@ -1,11 +1,50 @@
 ﻿namespace hw4
 
-    
-    
+module Task3 =
+    type LambdaTerm =
+        | Var of char
+        | App of LambdaTerm * LambdaTerm
+        | LambdaAbstr of LambdaTerm * char
 
+    let getFV (lambdaTerm: LambdaTerm) =
+        let rec accFV lambdaTerm acc =
+            match lambdaTerm with
+            | Var x ->  x::acc
+            | App(S, T) -> let lsFst = accFV S acc
+                           let lsSnd = accFV T acc
+                           List.append lsFst lsSnd
 
+            | LambdaAbstr(S, x) -> 
+                accFV S acc |> List.filter ((<>) x)
+        accFV lambdaTerm []
 
-    
-
-
-
+    let rec subst (lambdaTermFst: LambdaTerm) (lambdaTermSnd: LambdaTerm) var =
+        match lambdaTermFst with
+        | Var x -> if (x = var) then lambdaTermSnd else lambdaTermFst
+        | App(S, T) -> 
+            App(subst S lambdaTermSnd var, subst T lambdaTermSnd var)
+        
+        | LambdaAbstr(S, x) -> 
+            if (not(getFV lambdaTermSnd
+                    |> List.contains x && 
+                    getFV S 
+                    |> List.contains var))
+            then LambdaAbstr(subst S lambdaTermSnd var, x)
+            else
+                let z = 
+                    ['a'..'z'] 
+                    |> List.filter 
+                        (fun x ->
+                            not(List.contains x ((getFV lambdaTermSnd) @ (getFV S))))
+                    |> List.head
+                LambdaAbstr( subst (subst S (Var z) x) lambdaTermSnd var, z)
+            
+    let rec eval (lambdaTerm: LambdaTerm) =
+        match lambdaTerm with
+        | LambdaAbstr(S, x) -> LambdaAbstr(eval S, x)
+        | App (S, T) -> match eval S with 
+        ///вот это очень важно^^^^иначе потеряется рекурсивный вызов
+                        | LambdaAbstr(M, x) -> eval (subst M T x)
+                        | _ -> App(eval S, eval T)
+        | Var x -> Var x
+        
