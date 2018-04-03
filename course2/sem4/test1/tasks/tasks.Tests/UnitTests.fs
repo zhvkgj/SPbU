48c232cd7045e27acc6cf1e1af924e4cc17fda17
zhvkgj <zhvkgj@yandex.ru>
2018-04-03 11:18:54 +0300

    add task1

---
+++ course2/sem4/test1/tasks/tasks.Tests/UnitTests.fs
@@ -0,0 +1,28 @@
+﻿module Tests
+
+open NUnit.Framework
+open Tasks
+open Task1
+open Task2
+open System
+
+//task1 tests
+[<Test>]
+let ``Simple test1`` () =
+    let expected = Math.PI / 6.0
+    let actual = avgSum [Math.PI / 6.0; Math.PI / 6.0; Math.PI / 6.0]
+    Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``Simple test2`` () =
+    let expected = Math.PI / 2.0
+    let actual = avgSum [Math.PI / 2.0; Math.PI / 2.0; Math.PI / 2.0]
+    Assert.AreEqual(expected, actual)
+
+[<Test>]
+let ``Simple test3`` () =
+    let expected = Math.PI / 3.0
+    let actual = avgSum [Math.PI / 3.0; Math.PI / 3.0; Math.PI / 3.0]
+    Assert.AreEqual(expected, actual)
+
+//task2 tests
---
+++ course2/sem4/test1/tasks/tasks/Program.fs
@@ -0,0 +1,33 @@
+﻿namespace Tasks
+
+//task1
+module Task1 =
+    let avgSum ls =
+        let rec sumSin (ls: float list) (sum: float) (count: float) =
+            match ls with
+            | h::t -> sumSin t (sum + sin h) (count + 1.0)
+                  
+            | [] -> if (count = 0.0) then 0.0
+                    else sum / count
+    
+        sumSin ls 0.0 0.0
+
+//task2
+module Task2 =
+    type Tree<'a> =
+    | Node of 'a * Tree<'a> * Tree<'a>
+    | Empty 
+
+    let checkMin (x: int) (y: int) =
+        if (x < y) then x else y
+
+    type ContinuationStep<'a> = 
+        | Finished
+        | Step of 'a * (unit -> ContinuationStep<'a>)
+
+    let rec mapping tree contFunc =
+        match tree with 
+        | Empty -> contFunc()
+        | Node(x, left, right) -> Step(x, (fun () -> mapping left (fun () -> mapping right contFunc)))
+
+  
