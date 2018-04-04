bf7187cb4299fa1d74752ac3e78ebb153260adc5
zhvkgj <zhvkgj@yandex.ru>
2018-04-04 03:44:06 +0300

    add task1task2

---
+++ course2/sem4/hw7/tasks/tasks/Program.fs
@@ -0,0 +1,25 @@
+﻿namespace hw7
+
+module Task1 =
+    type RoundingBuilder(rnd: int) =
+        member this.Bind(x, f) = f x
+        member this.Return(x: float) =
+            let y = System.Math.Round (x, rnd)
+            y
+    
+    let rounding a = new RoundingBuilder(a)
+
+module Task2 =
+    open System
+    
+    type CalculateStringBuilder() =
+        member this.Bind(str, f) =
+            let mutable result = 0
+            match Int32.TryParse(str, &result) with
+            | true -> result |> f
+            | false -> None
+
+        member this.Return(x) =
+             Some x
+
+    let calculate() = new CalculateStringBuilder()    
