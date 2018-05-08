19cbd01bb487903eeafb2cfd9b688bfe4a3d9723
zhvkgj <zhvkgj@yandex.ru>
2018-05-08 18:31:47 +0300

    add task1 hw8

--- course2/sem4/hw8/task1/task1/Program.fs
+++ course2/sem4/hw8/task1/task1/Program.fs
@@ -2,29 +2,33 @@
 open System.IO
 open System.Text.RegularExpressions
   
-let fetchPageAsync url =
+let fetchPageAsync (url: string) =
     async {
-        do printfn "Creating request for %s..." url
         let request = WebRequest.Create(url)
         use! response = request.AsyncGetResponse()
-        do printfn "Getting response stream for %s.." url
         use stream = response.GetResponseStream()
-        do printfn "Reading response for %s.." url
         use reader = new StreamReader(stream)
         let html = reader.ReadToEnd()
         do printfn "%s : %d characters" url html.Length
         return html
     }
 
-let getAllHref url =
+let getRefFromHref (href: string) = 
+    let spl = href.Split('\"')
+    spl.[1]
+
+let getAllRefPage url =
     async {
         let! myPage = fetchPageAsync url
         let matches = Regex.Matches(myPage, @"<a href=""http://.+?"">")
-        for i in 0..matches.Count - 1 do 
-            printfn "%s" matches.[i].Value
-        
-    } |> Async.RunSynchronously |> ignore
+        let asyncList = [for href in matches -> 
+                            href.Value
+                            |> getRefFromHref 
+                            |> fetchPageAsync ]
+        let! result = Async.Parallel asyncList
+        return result
+        } |> Async.RunSynchronously |> ignore
 
-getAllHref "http://websharper.com/"
+getAllRefPage "http://websharper.com/"
 
 
