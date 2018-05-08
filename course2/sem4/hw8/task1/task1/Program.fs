57963e1808228c6c8cfb8b1d60fe04abd4ec809f
zhvkgj <zhvkgj@yandex.ru>
2018-05-08 10:10:47 +0300

    h8 update

---
+++ course2/sem4/hw8/task1/task1/Program.fs
@@ -0,0 +1,28 @@
+﻿open System.Net
+open System.IO
+open System.Text.RegularExpressions
+  
+let fetchPageAsync url =
+    async {
+        do printfn "Creating request for %s..." url
+        let request = WebRequest.Create(url)
+        use! response = request.AsyncGetResponse()
+        do printfn "Getting response stream for %s.." url
+        use stream = response.GetResponseStream()
+        do printfn "Reading response for %s.." url
+        use reader = new StreamReader(stream)
+        let html = reader.ReadToEnd()
+        do printfn "%s  %d characters" url html.Length
+        return html
+    }
+
+let getAllHref url =
+    async {
+        let! myPage = fetchPageAsync url
+        let matches = Regex.Matches(myPage, @"<a href=""http://.+?"">")
+
+    } |> Async.RunSynchronously
+
+getAllHref "http://matrixcalc.org/"
+
+
