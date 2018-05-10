4ff09184321b320e03ba40b909cd3543b51651f4
zhvkgj <zhvkgj@yandex.ru>
2018-05-10 03:12:03 +0300

    add task1 hw6

--- course2/sem4/hw6/task1/task.Tests/UnitTests.fs
+++ course2/sem4/hw6/task1/task.Tests/UnitTests.fs
@@ -1,3 +1,23 @@
-﻿module Tests
+﻿(*namespace hw6.Tests
 
-open Tasks
+open hw6
+open Task1
+open NUnit.Framework
+
+module Task1Test =
+    let random = new System.Random()
+    let computers = 
+        [Computer("linux", false, random); 
+         Computer("windows", true, random); 
+         Computer("masOS", false, random)]
+    
+    let matrix = 
+        [[false; true; true];
+         [true; false; true;]
+         [true; true; false]]
+
+    let network = new Network(computers, matrix)
+    
+    [<Test>]
+    let ``Test1``() =
+        let*)
--- course2/sem4/hw6/task1/task1/Program.fs
+++ course2/sem4/hw6/task1/task1/Program.fs
@@ -1,27 +1,81 @@
-﻿namespace Tasks
+﻿namespace hw6
 
 module Task1 =
     open System
 
-    type OS = 
-        { Name: string;
-          PossibilityOfInfection: float }
+    [<Measure>] type percent
 
-    type Computer(oSystem: OS, isInfected: bool, randNum: Random) =
-        let mutable infected = isInfected 
+    let random = new System.Random()
+
+    let (|Linux|_|) osName =
+        match osName with
+        | "linux" -> Some 35<percent>
+        | _ -> None
+    let (|Windows|_|) osName = 
+        match osName with 
+        | "windows" -> Some 70<percent>
+        | _ -> None
+    let (|MacOS|_|) osName =
+        match osName with
+        | "macOS" -> Some 25<percent>
+        | _ -> None
+
+    type Computer(os: string, isInfected: bool, randNum: Random) =
+        let mutable infected = isInfected
+        let mutable possibilityOfInfection = 0<percent>
+        do match os with
+           | Windows x | Linux x | MacOS x -> 
+                possibilityOfInfection <- x
+           | _ -> failwith "Unknown OS!"  
+
         member this.Infected 
             with get() = infected
-        member this.tryToInfect = 
-            infected <- (oSystem.PossibilityOfInfection >= randNum.NextDouble())
-
+        member this.TryToInfect = 
+            let rNum = randNum.Next(100)
+            infected <- (possibilityOfInfection >= rNum * 1<percent>)
+             
     type Network(computers: list<Computer>, matrix: list<list<bool>>) =
         let mutable computers = computers
-        let mutable infectedComp = []
-        member this.updateInfComp = 
-            
-        member this.makeStep =
-            
+        let mutable infectedComp = List.empty
+        let updateInfComp index =
+            if (computers.[index].Infected) then 
+                infectedComp <- index :: infectedComp           
+        do for i in 0..computers.Length - 1 do
+            updateInfComp i   
         
+        member this.MakeStep = 
+            for i in infectedComp do
+                for j in 0..matrix.[i].Length - 1 do
+                    if (matrix.[i].[j] && not(List.contains j infectedComp))
+                        then computers.[j].TryToInfect
+                             updateInfComp j
+        
+        member this.GetState =
+            for i in infectedComp do
+                printfn "Computer with a number %i is infected..." i
+
+        member this.CheckWork =
+            this.MakeStep
+            printfn "Current condition: "
+            this.GetState
+
+
+    let computers = 
+        [Computer("linux", false, random); 
+         Computer("windows", true, random); 
+         Computer("macOS", false, random)]
+    
+    let matrix = 
+        [[false; true; true];
+         [true; false; true;]
+         [true; true; false]]
+
+    let network = new Network(computers, matrix)
+    for i in 0..3 do
+        network.CheckWork
+
+//module Task2 = 
+    
 
 
 (*open System.Collections.Generic
