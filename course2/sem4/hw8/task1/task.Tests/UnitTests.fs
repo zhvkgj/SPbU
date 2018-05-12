df7e54bbec1c4c2915e30c952e687604e0ce1d7d
zhvkgj <zhvkgj@yandex.ru>
2018-05-12 20:13:28 +0300

    add unit test for hw8

---
+++ course2/sem4/hw8/task1/task.Tests/UnitTests.fs
@@ -0,0 +1,15 @@
+﻿module Tests
+
+open Hw8
+open ParsingPage
+open NUnit.Framework
+open FsUnit
+
+[<Test>]
+let ``Test for getting references from href``() =
+       getRefFromHref "<a href=\"http://fsharp.org/guides/slack/\" target=\"_blank\" title=\"F# Web Slack\">" 
+       |> should equal "http://fsharp.org/guides/slack/"
+       getRefFromHref "<a href=\"http://twitter.com/websharper\" class=\"button is-info\" style=\"background:#1b95e0\">"
+       |> should equal "http://twitter.com/websharper"
+
+    
